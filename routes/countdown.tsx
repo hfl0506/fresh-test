89b4cd4b0f22f72c367d825cf301dcf310953d6c
ronnieli <lihinfei0506@gmail.com>
2022-06-29 21:00:05 +0800

    deno fresh test

---
+++ routes/countdown.tsx
@@ -0,0 +1,14 @@
+/** @jsx h */
+
+import { h } from "preact";
+import CountDown from "../islands/Countdown.tsx";
+
+export default function Page() {
+  const date = new Date();
+  date.setHours(date.getHours() + 1);
+  return (
+    <p>
+      The big event is happening <CountDown target={date.toISOString()} />
+    </p>
+  );
+}
